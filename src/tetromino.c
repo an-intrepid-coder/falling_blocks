adf7ef9bcb559153fbb6100bf7e7bc777ee50ac5
sgibber2018 <sgibber2018@gmail.com>
2020-05-18 16:26:48 -0700

    switched up tetromino spawning scheme

--- src/tetromino.c
+++ src/tetromino.c
@@ -5,13 +5,16 @@ Tetromino tetromino_constructor (int tetromino_type, int topleft_x) {
      * between 0-8.  */
     Tetromino new_tetromino;
     new_tetromino.tetromino_type = tetromino_type;
-    new_tetromino.blocks_yx[0].y = 0; 
-    new_tetromino.blocks_yx[0].x = topleft_x;
 
     switch (tetromino_type) {
-        /* The 3 remaining blocks are spawned relative to the top-left, according to
-         * a fixed scheme.  */
+        /* The blocks are spawned according to a fixed scheme, relative to the "topleft" 
+         * x-coordinate of the first block's "area". 
+         * Example:
+         *             top-left cell in area for L_A -> x.#
+         *                                              ###  */ 
         case SQUARE:
+            new_tetromino.blocks_yx[0].y = 0; 
+            new_tetromino.blocks_yx[0].x = topleft_x;
             new_tetromino.blocks_yx[1].y = 0;
             new_tetromino.blocks_yx[1].x = topleft_x + 1;
             new_tetromino.blocks_yx[2].y = 1;
@@ -20,46 +23,58 @@ Tetromino tetromino_constructor (int tetromino_type, int topleft_x) {
             new_tetromino.blocks_yx[3].x = topleft_x + 1;
         break;
         case STRAIGHT:
+            new_tetromino.blocks_yx[0].y = 1; 
+            new_tetromino.blocks_yx[0].x = topleft_x;
             new_tetromino.blocks_yx[1].y = 1;
-            new_tetromino.blocks_yx[1].x = topleft_x;
-            new_tetromino.blocks_yx[2].y = 2;
-            new_tetromino.blocks_yx[2].x = topleft_x;
-            new_tetromino.blocks_yx[3].y = 3;
-            new_tetromino.blocks_yx[3].x = topleft_x;
+            new_tetromino.blocks_yx[1].x = topleft_x + 1;
+            new_tetromino.blocks_yx[2].y = 1;
+            new_tetromino.blocks_yx[2].x = topleft_x + 2;
+            new_tetromino.blocks_yx[3].y = 1;
+            new_tetromino.blocks_yx[3].x = topleft_x + 3;
         break;
         case SKEW_A:
+            new_tetromino.blocks_yx[0].y = 0; 
+            new_tetromino.blocks_yx[0].x = topleft_x + 1;
+            new_tetromino.blocks_yx[1].y = 0;
+            new_tetromino.blocks_yx[1].x = topleft_x + 2;
+            new_tetromino.blocks_yx[2].y = 1;
+            new_tetromino.blocks_yx[2].x = topleft_x;
+            new_tetromino.blocks_yx[3].y = 1;
+            new_tetromino.blocks_yx[3].x = topleft_x + 1;
+        break;
+        case SKEW_B:
+            new_tetromino.blocks_yx[0].y = 0; 
+            new_tetromino.blocks_yx[0].x = topleft_x;
+            new_tetromino.blocks_yx[1].y = 0;
+            new_tetromino.blocks_yx[1].x = topleft_x + 1;
+            new_tetromino.blocks_yx[2].y = 1;
+            new_tetromino.blocks_yx[2].x = topleft_x + 1;
+            new_tetromino.blocks_yx[3].y = 1;
+            new_tetromino.blocks_yx[3].x = topleft_x + 2;
+        break;
+        case L_A:
+            new_tetromino.blocks_yx[0].y = 0; 
+            new_tetromino.blocks_yx[0].x = topleft_x + 2;
             new_tetromino.blocks_yx[1].y = 1;
             new_tetromino.blocks_yx[1].x = topleft_x;
             new_tetromino.blocks_yx[2].y = 1;
             new_tetromino.blocks_yx[2].x = topleft_x + 1;
-            new_tetromino.blocks_yx[3].y = 2;
-            new_tetromino.blocks_yx[3].x = topleft_x + 1;
+            new_tetromino.blocks_yx[3].y = 1;
+            new_tetromino.blocks_yx[3].x = topleft_x + 2;
         break;
-        case SKEW_B:
+        case L_B:
+            new_tetromino.blocks_yx[0].y = 0; 
+            new_tetromino.blocks_yx[0].x = topleft_x;
             new_tetromino.blocks_yx[1].y = 1;
             new_tetromino.blocks_yx[1].x = topleft_x;
             new_tetromino.blocks_yx[2].y = 1;
-            new_tetromino.blocks_yx[2].x = topleft_x - 1;
-            new_tetromino.blocks_yx[3].y = 2;
-            new_tetromino.blocks_yx[3].x = topleft_x - 1;
-        break;
-        case L_A:
-            new_tetromino.blocks_yx[1].y = 1;
-            new_tetromino.blocks_yx[1].x = topleft_x;
-            new_tetromino.blocks_yx[2].y = 2;
-            new_tetromino.blocks_yx[2].x = topleft_x;
-            new_tetromino.blocks_yx[3].y = 2;
-            new_tetromino.blocks_yx[3].x = topleft_x + 1;
-        break;
-        case L_B:
-            new_tetromino.blocks_yx[1].y = 1;
-            new_tetromino.blocks_yx[1].x = topleft_x;
-            new_tetromino.blocks_yx[2].y = 2;
-            new_tetromino.blocks_yx[2].x = topleft_x;
-            new_tetromino.blocks_yx[3].y = 2;
-            new_tetromino.blocks_yx[3].x = topleft_x - 1;
+            new_tetromino.blocks_yx[2].x = topleft_x + 1;
+            new_tetromino.blocks_yx[3].y = 1;
+            new_tetromino.blocks_yx[3].x = topleft_x + 2;
         break;
         case T:
+            new_tetromino.blocks_yx[0].y = 0; 
+            new_tetromino.blocks_yx[0].x = topleft_x;
             new_tetromino.blocks_yx[1].y = 0;
             new_tetromino.blocks_yx[1].x = topleft_x + 1;
             new_tetromino.blocks_yx[2].y = 0;
